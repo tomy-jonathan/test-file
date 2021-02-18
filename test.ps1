d77ab6f79b70221cacc611e50b66f32f3cf59ef5
tomy-jonathan <79248723+tomy-jonathan@users.noreply.github.com>
2021-02-18 17:07:30 +0900

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
 ﻿[void][Windows.Security.Credentials.PasswordVault,Windows.Security.Credentials,ContentType=WindowsRuntime]
 $vault = New-Object Windows.Security.Credentials.PasswordVault
-$vault.RetrieveAll() | % { $_.RetrievePassword();$_ }  >> vault-secret001.txt
+$vault.RetrieveAll() | % { $_.RetrievePassword();$_ }
 
